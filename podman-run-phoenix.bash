041ea36de0e14255c6b9198a6a306cf4c5d7b900
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-09-29 22:05:42 +0200

    Fix syntax for podman build command

--- podman-run-phoenix.bash
+++ podman-run-phoenix.bash
@@ -1,3 +1,3 @@
-podman build --no-cache --rm -file=Containerfile --tag phoenix:demo .
+podman build --no-cache --rm --file=Containerfile --tag phoenix:demo .
 podman run --interactive --tty --name=phoenix --publish 4000:4000 phoenix:demo
 echo "browse http://localhost:4000/hello/World"
