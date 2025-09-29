752cdfef8e7e3d49c0f263531a5442c4a6a49ad9
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-09-29 20:56:06 +0200

    Update echo statement to new URL

--- podman-run-phoenix.bash
+++ podman-run-phoenix.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t phoenix:demo .
 podman run --interactive --tty -p 4000:4000 phoenix:demo
-echo "browse http://localhost:4000/"
+echo "browse http://localhost:4000/hello/World"
