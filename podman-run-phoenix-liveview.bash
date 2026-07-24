ba3227ccaf814e19b34d74bb7cb71147353e0935
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-07-24 12:12:53 +0200

    Update podman run command and echo statements

--- podman-run-phoenix-liveview.bash
+++ podman-run-phoenix-liveview.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm --file=Containerfile.LiveView --tag phoenix:liveview .
-podman run --interactive --tty --name=phoenix.liveview --replace --publish 4003:4003 --publish 4004:4004 phoenix:liveview
-echo "browse http://localhost:4003/tasks or https://localhost:4004/tasks"
+podman run --interactive --tty --name=phoenix.liveview --replace --publish 4004:4004 phoenix:liveview
+echo "browse https://localhost:4004/tasks"
